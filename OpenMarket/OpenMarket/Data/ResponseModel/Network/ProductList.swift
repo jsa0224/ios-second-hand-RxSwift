09ac617b52c844ab89603ea2bc942a3be3c5afa6
jsa0224 <voidness45@gmail.com>
2023-05-26 10:59:38 +0900

    feat: NetworkRepository 객체 추가

--- OpenMarket/OpenMarket/Data/ResponseModel/Network/ProductList.swift
+++ OpenMarket/OpenMarket/Data/ResponseModel/Network/ProductList.swift
@@ -6,27 +6,13 @@
 //
 
 struct ProductList: Codable {
-    let pageNo: Int
-    let itemsPerPage: Int
-    let totalCount: Int
-    let offset: Int
-    let limit: Int
-    let lastPage: Int
-    let hasNext: Bool
-    let hasPrev: Bool
-    let pages: [Product]
-}
-
-extension ProductList {
-    func toDomain() -> ItemList {
-        return ItemList(pageNo: self.pageNo,
-                        itemsPerPage: self.itemsPerPage,
-                        totalCount: self.totalCount,
-                        offset: self.offset,
-                        limit: self.limit,
-                        lastPage: self.lastPage,
-                        hasNext: self.hasNext,
-                        hasPrev: self.hasPrev,
-                        pages: self.pages.map { $0.toDomain() })
-    }
+    let pageNo: Int?
+    let itemsPerPage: Int?
+    let totalCount: Int?
+    let offset: Int?
+    let limit: Int?
+    let lastPage: Int?
+    let hasNext: Bool?
+    let hasPrev: Bool?
+    let pages: [Product]?
 }
---
+++ OpenMarket/OpenMarket/Domain/Interfaces/Repositories/NetworkRepository.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkRepository.swift
+//  OpenMarket
+//
+//  Created by 정선아 on 2023/05/24.
+//
+
+import Foundation
+import RxSwift
+
+protocol NetworkRepository {
+    func fetchItemList(pageNo: Int, itemPerPage: Int) -> Observable<[Item]>
+    func fetchItemImage(url: String) -> Observable<Data>
+}
